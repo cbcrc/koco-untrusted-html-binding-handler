2b42a3f56794a9b2a37403c90744a143dab5bb65
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:23:59 -0400

    update to koco2

--- src/untrusted-html-binding-handler.js
+++ src/untrusted-html-binding-handler.js
@@ -1,26 +1,27 @@
-define(['knockout', 'jquery', 'string-utilities', 'i18next'],
-    function(ko, $, stringUtilities, i18n) {
-        'use strict';
+import ko from 'knockout';
+import $ from 'jquery';
+import stringUtilities from 'string-utilities';
+import i18n from 'i18next';
 
-        ko.bindingHandlers.untrustedHtml = {
-            update: function(element, valueAccessor, allBindingsAccessor) {
-                
-                var value = ko.unwrap(valueAccessor());
-                var incomingSettings = value.settings;
-                
-                var defaultSettings = {
-                    defaultText: i18n.t('koco-untrusted-html-binding-handler.defaultText')
-                };
-                var settings = $.extend({}, defaultSettings, incomingSettings);
 
-                ko.bindingHandlers.html.update(element,
-                    function() {
-                        var su = stringUtilities;
-                        var text = ko.unwrap(value.text);
-                        var santitizedValue = text ? su.stripHtmlFromText(text) : settings.defaultText;
-                        var trimmedValue = su.trimRight(santitizedValue);
-                        return trimmedValue;
-                    });
-            }
+ko.bindingHandlers.untrustedHtml = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+
+        var value = ko.unwrap(valueAccessor());
+        var incomingSettings = value.settings;
+
+        var defaultSettings = {
+            defaultText: i18n.t('koco-untrusted-html-binding-handler.defaultText')
         };
-});
+        var settings = $.extend({}, defaultSettings, incomingSettings);
+
+        ko.bindingHandlers.html.update(element,
+            function() {
+                var su = stringUtilities;
+                var text = ko.unwrap(value.text);
+                var santitizedValue = text ? su.stripHtmlFromText(text) : settings.defaultText;
+                var trimmedValue = su.trimRight(santitizedValue);
+                return trimmedValue;
+            });
+    }
+};
