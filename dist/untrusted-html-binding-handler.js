53f82652bb3992e210adcbfdd2a450d1b1345c94
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-26 16:59:57 -0400

    patch

---
+++ dist/untrusted-html-binding-handler.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var _knockout = require('knockout');
+
+var _knockout2 = _interopRequireDefault(_knockout);
+
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+var _kocoStringUtilities = require('koco-string-utilities');
+
+var _kocoStringUtilities2 = _interopRequireDefault(_kocoStringUtilities);
+
+var _i18next = require('i18next');
+
+var _i18next2 = _interopRequireDefault(_i18next);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_knockout2.default.bindingHandlers.untrustedHtml = {
+    update: function update(element, valueAccessor, allBindingsAccessor) {
+
+        var value = _knockout2.default.unwrap(valueAccessor());
+        var incomingSettings = value.settings;
+
+        var defaultSettings = {
+            defaultText: _i18next2.default.t('koco-untrusted-html-binding-handler.defaultText')
+        };
+        var settings = _jquery2.default.extend({}, defaultSettings, incomingSettings);
+
+        _knockout2.default.bindingHandlers.html.update(element, function () {
+            var su = _kocoStringUtilities2.default;
+            var text = _knockout2.default.unwrap(value.text);
+            var santitizedValue = text ? su.stripHtmlFromText(text) : settings.defaultText;
+            var trimmedValue = su.trimRight(santitizedValue);
+            return trimmedValue;
+        });
+    }
+};
